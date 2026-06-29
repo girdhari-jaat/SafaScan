bbe3e3490bf727d88f42bca285db0457d3c0ce83
girdhari-jaat <girdharijaat@gmail.com>
2026-06-29 10:33:56 +0500

    Fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -98,10 +98,11 @@ dependencies {
     implementation("androidx.camera:camera-view:$camerax_version")
 
     // ML Kit
+    implementation(platform("com.google.mlkit:mlkit-bom:33.1.2"))
     implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
     implementation("com.google.mlkit:object-detection:17.0.2")
-    implementation("com.google.mlkit:document-scanner:16.0.0-beta4")
+    implementation("com.google.mlkit:document-scanner")
 
     // Lifecycle/ViewModel/Coroutines
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
